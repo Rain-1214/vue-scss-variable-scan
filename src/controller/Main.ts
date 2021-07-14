a91ede35404a4df4e298895da0e820b8e649234c
wanghao <wanghao1@actionsky.com>
2021-07-14 18:25:20 +0800

    [feature]: find color value when variable value is another variable

--- src/controller/Main.ts
+++ src/controller/Main.ts
@@ -1,10 +1,20 @@
-import { Disposable, Uri, workspace, languages, TextDocument, Position, CompletionItem, Range, CompletionItemKind, ExtensionContext } from "vscode";
-import Tool from "./Tool";
-import Notifier from "./Notifier";
-import ScssVariable from "../entity/ScssVariable";
+import {
+  Disposable,
+  Uri,
+  workspace,
+  languages,
+  TextDocument,
+  Position,
+  CompletionItem,
+  Range,
+  CompletionItemKind,
+  ExtensionContext,
+} from 'vscode';
+import Tool from './Tool';
+import Notifier from './Notifier';
+import ScssVariable from '../entity/ScssVariable';
 
 class Main {
-
   private static instance: Main;
 
   public static getInstance() {
@@ -19,32 +29,50 @@ class Main {
   private scssVariables: ScssVariable[] = [];
   private supportFileType: string[] = ['vue', 'scss'];
   public resourceCheckCssProperty: string[] = [
-    "background", "border", "color", "width", "height", "font-size","padding", "margin"
+    'background',
+    'border',
+    'color',
+    'width',
+    'height',
+    'font-size',
+    'padding',
+    'margin',
   ];
   public checkCssProperty: string[] = [
-    "background", "border", "color", "width", "height", "font-size","padding", "margin"
+    'background',
+    'border',
+    'color',
+    'width',
+    'height',
+    'font-size',
+    'padding',
+    'margin',
   ];
-  
 
-  private constructor () {}
+  private constructor() {}
 
   public async scanScssVariable() {
     this.scssVariables = [];
-    this.notifier.notify("eye", "Looking for CSS classes in the workspace...");
+    this.notifier.notify('eye', 'Looking for CSS classes in the workspace...');
     const disposables: Disposable[] = [];
     const config = workspace.getConfiguration();
     const globalPath = config.get('vue-scss-variable-scan.globalPath');
-    const globalExcludePath = config.get('vue-scss-variable-scan.globalExcludePath');
-    const uris: Uri[] = await this.tool.findAllFileByPathAndExcPath(`${globalPath}`, `${globalExcludePath}`);
+    const globalExcludePath = config.get(
+      'vue-scss-variable-scan.globalExcludePath'
+    );
+    const uris: Uri[] = await this.tool.findAllFileByPathAndExcPath(
+      `${globalPath}`,
+      `${globalExcludePath}`
+    );
     if (!uris || uris.length === 0) {
       this.notifier.statusBarItem.hide();
       return disposables;
     }
 
-    for(let i = 0;i < uris.length; i++) {
+    for (let i = 0; i < uris.length; i++) {
       const text = await this.tool.readeFile(uris[i].fsPath);
       const allLines: string[] = text.split('\n');
-      for(let y = 0; y < allLines.length; y++) {
+      for (let y = 0; y < allLines.length; y++) {
         const line = allLines[y];
         if (line === '' || !line.includes(';')) {
           continue;
@@ -56,49 +84,98 @@ class Main {
             continue;
           }
           const key = value.split(':')[0].trim();
+
           if (key.startsWith('$')) {
-            this.scssVariables.push(new ScssVariable(key, value.split(':')[1].trim()));
+            console.log(key, value.split(':')[1].trim());
+            this.scssVariables.push(
+              new ScssVariable(key, value.split(':')[1].trim())
+            );
           }
         }
       }
     }
-    this.notifier.notify('eye', `Looking for scss variable completed and find ${this.scssVariables.length} variable`);
+    for (let i = 0; i < this.scssVariables.length; i++) {
+      const currentVar = this.scssVariables[i];
+      if (currentVar.value.startsWith('$')) {
+        currentVar.value = this.getColorByVariable(currentVar.value, 0);
+      }
+    }
+    this.notifier.notify(
+      'eye',
+      `Looking for scss variable completed and find ${this.scssVariables.length} variable`
+    );
+  }
+
+  public getColorByVariable(variable: string, deep: number): string {
+    if (deep > 100) {
+      return '';
+    }
+    const currentVariable = this.scssVariables.find(
+      (item) => item.variable === variable
+    );
+    if (!!currentVariable) {
+      if (currentVariable.value.startsWith('$')) {
+        return this.getColorByVariable(currentVariable.value, deep + 1);
+      } else {
+        return currentVariable.value;
+      }
+    }
+    return variable;
   }
 
   public createDisposables(context: ExtensionContext) {
-    for(let i = 0; i < this.supportFileType.length; i++) {
-      context.subscriptions.push(this.registerCompletionItem(this.supportFileType[i], /:([\s\S]*)/));
+    for (let i = 0; i < this.supportFileType.length; i++) {
+      context.subscriptions.push(
+        this.registerCompletionItem(this.supportFileType[i], /:([\s\S]*)/)
+      );
     }
   }
 
-  public registerCompletionItem (languageSelector: string, matchRegex: RegExp, classPrefix: string = "", splitChar: string = " ") {
+  public registerCompletionItem(
+    languageSelector: string,
+    matchRegex: RegExp,
+    classPrefix: string = '',
+    splitChar: string = ' '
+  ) {
     const _this = this;
-    return languages.registerCompletionItemProvider({ scheme: 'file', language: languageSelector }, {
-      provideCompletionItems(document: TextDocument, position: Position): Thenable<CompletionItem[]> {
+    return languages.registerCompletionItemProvider(
+      { scheme: 'file', language: languageSelector },
+      {
+        provideCompletionItems(
+          document: TextDocument,
+          position: Position
+        ): Thenable<CompletionItem[]> {
           const start = new Position(position.line, 0);
           const range: Range = new Range(start, position);
           const text: string = document.getText(range);
-  
+
           const rawText: RegExpMatchArray | null = text.match(matchRegex);
           if (!rawText) {
             return _this.tool.promiseFactory([]);
           }
           const cssProperty = text.split(':')[0].trim();
-          if (_this.checkCssProperty.find(e => cssProperty.includes(e)) === undefined) {
+          if (
+            _this.checkCssProperty.find((e) => cssProperty.includes(e)) ===
+            undefined
+          ) {
             return _this.tool.promiseFactory([]);
           }
           const items = [];
-          for(let i = 0; i < _this.scssVariables.length; i++) {
-            const item = new CompletionItem(_this.scssVariables[i].variable, CompletionItemKind.Color);
+          for (let i = 0; i < _this.scssVariables.length; i++) {
+            const item = new CompletionItem(
+              _this.scssVariables[i].variable,
+              CompletionItemKind.Color
+            );
             item.filterText = _this.scssVariables[i].variable;
             item.detail = _this.scssVariables[i].value;
             items.push(item);
           }
           return _this.tool.promiseFactory(items);
-      }
-    }, ":");
+        },
+      },
+      ':'
+    );
   }
-
 }
 
 export default Main;
--- src/extension.ts
+++ src/extension.ts
@@ -2,53 +2,75 @@
 import * as vscode from 'vscode';
 import Main from './controller/Main';
 
-const scssFileChange = vscode.workspace.createFileSystemWatcher('**/*.scss', false, false, false);
+const scssFileChange = vscode.workspace.createFileSystemWatcher(
+  '**/*.scss',
+  false,
+  false,
+  false
+);
 
 export async function activate(context: vscode.ExtensionContext) {
-    const main = Main.getInstance();
+  const main = Main.getInstance();
+  await main.scanScssVariable();
+  main.createDisposables(context);
+
+  const disposables: vscode.Disposable[] = [];
+  vscode.workspace.onDidChangeConfiguration(
+    async (e) => {
+      if (
+        e.affectsConfiguration('vue-scss-variable-scan.globalPath') ||
+        e.affectsConfiguration('vue-scss-variable-scan.globalExcludePath')
+      ) {
+        await main.scanScssVariable();
+      }
+      if (
+        e.affectsConfiguration('vue-scss-variable-scan.addCustomCssProperty')
+      ) {
+        const newCssProperty = vscode.workspace
+          .getConfiguration()
+          .get('vue-scss-variable-scan.addCustomCssProperty') as string[];
+        main.checkCssProperty = [
+          ...newCssProperty,
+          ...main.resourceCheckCssProperty,
+        ];
+      }
+      if (e.affectsConfiguration('vue-scss-variable-scan.excludeCssProperty')) {
+        const excludeCssProperty = vscode.workspace
+          .getConfiguration()
+          .get('vue-scss-variable-scan.excludeCssProperty') as string[];
+        main.checkCssProperty = main.resourceCheckCssProperty.filter(
+          (e) => !excludeCssProperty.includes(e)
+        );
+      }
+    },
+    null,
+    disposables
+  );
+
+  scssFileChange.onDidCreate(async (e) => {
     await main.scanScssVariable();
-    main.createDisposables(context);
-    
-    const disposables: vscode.Disposable[] = [];
-    vscode.workspace.onDidChangeConfiguration(async (e) => {
-        if (e.affectsConfiguration("vue-scss-variable-scan.globalPath") ||
-            e.affectsConfiguration("vue-scss-variable-scan.globalExcludePath")) {
-                await main.scanScssVariable();
-            }
-        if (e.affectsConfiguration("vue-scss-variable-scan.addCustomCssProperty")) {
-            const newCssProperty = vscode.workspace.getConfiguration().get('vue-scss-variable-scan.addCustomCssProperty') as string[];
-            main.checkCssProperty = [ ...newCssProperty, ...main.resourceCheckCssProperty ];
-            await main.createDisposables(context);
-        }
-        if (e.affectsConfiguration("vue-scss-variable-scan.excludeCssProperty")) {
-            const excludeCssProperty = vscode.workspace.getConfiguration().get('vue-scss-variable-scan.excludeCssProperty') as string[];
-            main.checkCssProperty = main.resourceCheckCssProperty.filter(e => !excludeCssProperty.includes(e));
-            await main.createDisposables(context);
-        }
-    }, null, disposables);
+  });
 
-    scssFileChange.onDidCreate(async (e) => {
+  scssFileChange.onDidChange(async (e) => {
+    await main.scanScssVariable();
+  });
+  scssFileChange.onDidDelete(async (e) => {
+    await main.scanScssVariable();
+  });
+
+  context.subscriptions.push(...disposables);
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      'vue-scss-variable-scan.refresh',
+      async () => {
         await main.scanScssVariable();
-    });
-
-    scssFileChange.onDidChange(async (e) => {
-        await main.scanScssVariable();
-    });
-    scssFileChange.onDidDelete(async (e) => {
-        await main.scanScssVariable();
-    });
-
-    context.subscriptions.push(...disposables);
-
-    context.subscriptions.push(vscode.commands.registerCommand("vue-scss-variable-scan.refresh", async () => {
-        await main.scanScssVariable();
-        main.createDisposables(context);
-    }));
-
-
+      }
+    )
+  );
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
-    scssFileChange.dispose();
+  scssFileChange.dispose();
 }
